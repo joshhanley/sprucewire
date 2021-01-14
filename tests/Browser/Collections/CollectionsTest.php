ff41cf2434f576aef84a74e1a5e23231a61e9074
Josh Hanley <josh@hitsystems.com.au>
2021-01-14 14:21:34 +1100

    Add failing collections alternating test

--- tests/Browser/Collections/CollectionsTest.php
+++ tests/Browser/Collections/CollectionsTest.php
@@ -75,4 +75,59 @@ class CollectionsTest extends TestCase
                     ;
         });
     }
+
+    /** @test */
+    public function it_can_add_and_remove_collection_elements_alternating()
+    {
+        $this->browse(function (Browser $browser) {
+            Livewire::visit($browser, CollectionsComponent::class)
+                    // Check all starting values are ok
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4])
+
+                    // Test changing collections swapping back and forth
+                    ->waitForLivewire()->click('@livewire-add')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4,5])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4,5])
+                    ->waitForLivewire()->click('@spruce-add')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4,5,6])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4,5,6])
+                    ->waitForLivewire()->click('@spruce-remove')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4,5])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4,5])
+                    ->assertDontSeeIn('@livewire-collection-output', 6)
+                    ->assertDontSeeIn('@spruce-collection-output', 6)
+                    ->waitForLivewire()->click('@livewire-remove')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4])
+                    ->assertDontSeeIn('@livewire-collection-output', 5)
+                    ->assertDontSeeIn('@spruce-collection-output', 5)
+                    ->waitForLivewire()->click('@spruce-add')
+                    ->waitForLivewire()->click('@spruce-add')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4,5,6])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4,5,6])
+                    ->waitForLivewire()->click('@livewire-add')
+                    ->waitForLivewire()->click('@livewire-add')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4,5,6,7,8])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4,5,6,7,8])
+                    ->waitForLivewire()->click('@livewire-remove')
+                    ->waitForLivewire()->click('@livewire-remove')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4,5,6])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4,5,6])
+                    ->assertDontSeeIn('@livewire-collection-output', 8)
+                    ->assertDontSeeIn('@spruce-collection-output', 8)
+                    ->assertDontSeeIn('@livewire-collection-output', 7)
+                    ->assertDontSeeIn('@spruce-collection-output', 7)
+                    ->waitForLivewire()->click('@spruce-remove')
+                    ->waitForLivewire()->click('@spruce-remove')
+                    ->assertSeeInOrder('@livewire-collection-output', [1,2,3,4])
+                    ->assertSeeInOrder('@spruce-collection-output', [1,2,3,4])
+                    ->assertDontSeeIn('@livewire-collection-output', 6)
+                    ->assertDontSeeIn('@spruce-collection-output', 6)
+                    ->assertDontSeeIn('@livewire-collection-output', 5)
+                    ->assertDontSeeIn('@spruce-collection-output', 5)
+
+                    ;
+        });
+    }
 }
